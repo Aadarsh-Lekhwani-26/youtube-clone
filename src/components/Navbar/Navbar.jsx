cc2e2e4203d984c8813f5b229dffec873c58df5f
Aadarsh Lekhwani <aadarshlekhwani@Aadarshs-MacBook-Pro.local>
2026-04-25 12:29:33 +0530

    added tailwind css and updated search Bar UI

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,12 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <h2>StreamVault</h2>
+      <h2 className="text-rose-500 text-2xl">StreamVault</h2>
 
-      <input type="text" placeholder="Type to search" />
+      <div className="flex">
+      <input type="text" placeholder="Type to search" className="text-xl rounded-bl-3xl rounded-tl-3xl bg-rose-500 border border-white"/>
+      <button className=" w-9 text-4xl rounded-tr-3xl rounded-br-3xl text-white bg-rose-500 p-3 border border-white hover:cursor-pointer ml-3" >⌕</button>
+      </div>
 
       <div>
         <Link to="/">Home</Link>
